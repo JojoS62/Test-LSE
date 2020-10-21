e2559dd8ca281ed4829e4b09eb5fe9167fd53169
JojoS62 <sn2010@onlinehome.de>
2020-10-21 19:42:07 +0200

    added F4xx targets with different settings added 'L' command to toggle Led and perform sleep_for()

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "mbed.h"
 
+#if defined(STM32F410Tx) || defined(STM32F410Cx) || defined(STM32F410Rx) || defined(STM32F411xE) || defined(STM32F446xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F412Zx) ||\
+    defined(STM32F412Vx) || defined(STM32F412Rx) || defined(STM32F412Cx) || defined(STM32F413xx) || defined(STM32F423xx)
+constexpr uint32_t lse_drive_levels[] = {
+    RCC_LSE_LOWPOWER_MODE,
+    RCC_LSE_HIGHDRIVE_MODE
+};
+#else
 constexpr uint32_t lse_drive_levels[] = {
     RCC_LSEDRIVE_LOW,
     RCC_LSEDRIVE_MEDIUMLOW,
     RCC_LSEDRIVE_MEDIUMHIGH,
     RCC_LSEDRIVE_HIGH
 };
+#endif
 
+
+DigitalOut  led1(LED1, 1);
 
 void enableLSE(bool enable)
 {
@@ -33,9 +43,14 @@ void setLSE_DriveLoad(uint level)
     // drive level can only be changed when LSE is off
     enableLSE(false);
 
+#if defined(STM32F410Tx) || defined(STM32F410Cx) || defined(STM32F410Rx) || defined(STM32F411xE) || defined(STM32F446xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F412Zx) ||\
+    defined(STM32F412Vx) || defined(STM32F412Rx) || defined(STM32F412Cx) || defined(STM32F413xx) || defined(STM32F423xx)
+    HAL_RCCEx_SelectLSEMode(lse_drive_levels[level]);
+#else
     // set LSE drive level
     HAL_PWR_EnableBkUpAccess();
     __HAL_RCC_LSEDRIVE_CONFIG(lse_drive_levels[level]);
+#endif
 
     // drive level can only be changed when LSE is off
     enableLSE(true);
@@ -51,7 +66,7 @@ int main() {
     while (1) {
         char ch = getc(stdin);
 
-        switch (ch)
+        switch (toupper(ch))
         {
         case '0':
             enableLSE(false);
@@ -60,8 +75,15 @@ int main() {
             enableLSE(true);
             break;
 
+        case 'L':
+            // this enables LSE when called the first time
+            ThisThread::sleep_for(100ms);
+            led1 = !led1;
+            ThisThread::sleep_for(100ms);
+            break;
+
         default:
-            if ((ch >= '1') && (ch <= '4')) {
+            if ((ch >= '1') && (ch <  '1' + sizeof(lse_drive_levels)/sizeof(lse_drive_levels[0]))) {
                 setLSE_DriveLoad(ch - '1');
             }
         }
